b918cda2108eb691d7f6f8a77ebaa4952b4a5f7a
Jared Porter <jaredp17@me.com>
2020-07-30 17:02:40 -0400

    Finished caches.

--- public/service-worker.js
+++ public/service-worker.js
@@ -24,11 +24,9 @@ self.addEventListener("install", function (evt) {
         cache
           .addAll(FILES_TO_CACHE)
           .then((result) => {
-            // debugger;
             console.log("result of add all", result);
           })
           .catch((err) => {
-            // debugger;
             console.log("Add all error: ", err);
           });
       })
